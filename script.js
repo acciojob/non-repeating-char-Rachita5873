4a5feca0355648d21fa54b227b96adebc1dc292b
Rachita5873 <76832946+Rachita5873@users.noreply.github.com>
2023-07-16 00:49:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 
 function result(key){
 	for(let i=0; i<key.length; i++){
-		if(key.indexOf(key[i])==key.lastIndexOf(key[i])){
+		if(key.indexOf(key[i])===key.lastIndexOf(key[i])){
 			return key[i];
 		}
 	}
-	return "";
+	return null
 }
 const key = prompt("Enter Key.");
 alert(result(key));
